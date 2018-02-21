78e9c7be40bd63a1fcb3f9e33cba663098e68f87
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-02-21 19:58:34 +0100

    :sparkles: Add 'getDirs' Function

--- index.ts
+++ index.ts
@@ -1,7 +1,35 @@
 import { IDublette, IDublettenpruefung, Vergleichsmodi } from './contracts/index';
+import * as isDirectory from 'is-directory';
+import * as fs from 'fs';
 
 test();
 
 function test():void {
   console.log('test');
 }
+
+function getDirs(path){
+  var found = fs.readdirSync(path);
+  var files = [];
+
+  for (const current of found){
+    const isFolder = isDirectory.sync(path+current);
+
+    if(isFolder){
+      var rec_found = getDirs(path+current+"/");
+      if(rec_found){
+        files = files.concat(rec_found);
+      }
+    }else{
+      files.push(current);
+    }
+  }
+
+  return files;
+}
+
+function logDirs(){
+  var data = getDirs('test/');
+  console.log("Folders: ", data);
+}
+logDirs();
